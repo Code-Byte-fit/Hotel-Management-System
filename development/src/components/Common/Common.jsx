4703bd55c4b962e569532a687a7b87512025272f
Shamly-Shanawaz <113548974+Shamly-Shanawaz@users.noreply.github.com>
2023-01-24 18:07:59 +0530

    Update Common.jsx

--- development/src/components/Common/Common.jsx
+++ development/src/components/Common/Common.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header/Header";
-import style from "./common.module.css"
+import style from "./Common.module.css"
 function Common(props){
     return(
         <>
